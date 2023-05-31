438be8334e513fb935fef313e78180a9233d6ec5
Shaikh Mudassir <send2mudassir@gmail.com>
2023-05-31 23:01:58 +0530

    Color Update
    Removed Copyrighted elements like URLs

--- yt.py
+++ yt.py
@@ -3,21 +3,20 @@ from tkinter import *
 from tkinter import messagebox
 from tkinter import filedialog
 from tkinter import ttk
-from tkinter.ttk import *
 from pytube import YouTube
 
-path1 = ""
-def browsefile():
-    global path1
-    path1 = filedialog.askdirectory()
-    lbl_path_show = Label(root, text= "Path: "+ str(path1), background = "gray3", foreground = "white", font = ("Bookman Old Style", 10, "bold"))
+selected_path = ""
+def browse_directory():
+    global selected_path
+    selected_path = filedialog.askdirectory()
+    lbl_path_show = Label(root, text= "Path: "+ str(selected_path), background = "lightgray", foreground = "black", font = ("Arial", 10, "bold"))
     lbl_path_show.place(x = 200, y = 162)
-    if len(path1) < 1:
+    if len(selected_path) < 1:
         messagebox.showerror("Error", "Please insert Directory") 
 
-def download():
+def download_video():
     url = str(ent_link.get())
-    res = var.get()
+    resolution = var.get()
 
     if len(url) < 1:
         messagebox.showerror("Error", "URL cannot be Empty")
@@ -25,20 +24,18 @@ def download():
     yt = YouTube(url)
     try:
         if var.get() == 0:
-             reso_select = yt.streams.get_highest_resolution()
+             selected_resolution = yt.streams.get_highest_resolution()
         elif var.get() == 1:
-            reso_select = yt.streams.get_lowest_resolution()
+            selected_resolution = yt.streams.get_lowest_resolution()
         elif var.get() == 2:
-            reso_select = yt.streams.filter(only_audio=True).first()
+            selected_resolution = yt.streams.filter(only_audio=True).first()
         else:
-            reso_select = yt.streams.get_highest_resolution()
+            selected_resolution = yt.streams.get_highest_resolution()
         try:
-            reso_select.download(path1)
+            selected_resolution.download(selected_path)
             
-            messagebox.showinfo("Sucess", "Video Downloaded!")
-            messagebox.showinfo("Follow Me", "For more Stuffs like this Follow me")
-            messagebox.showinfo("GitHub", "https://github.com/Sumant-Dusane")
-
+            messagebox.showinfo("Success", "Video Downloaded!")
+            messagebox.showinfo("Message", "For more information, please refer to the documentation.")
 
         except:
             messagebox.showerror("Error", "Download Failed")
@@ -49,19 +46,18 @@ def download():
 root = Tk()
 root.geometry('500x400+350+100')
 root.resizable(False, False)
-root.call('wm', 'iconphoto', root._w, PhotoImage(file='F:\Sumant\Python\YouTube Downloader\yicon.png'))
-root.title("Youtube Video Downloader")
-root.config(bg = "gray3")
+root.title("Video Downloader")
+root.config(bg = "lightgray")
 
 #--------------------------Header----------------------
-heading = Label(root, text = "Youtube Video Downloader", background  = "gray3", foreground = "#ff0000", font = ("Vadher", 15))
+heading = Label(root, text = "Video Downloader", background  = "lightgray", foreground = "blue", font = ("Arial", 15))
 heading.pack(anchor= "center", pady = 10)
 
-credit1 = Label(root, text = "-By Sumant Dusane ❤", background  = "gray3", foreground = "white", font = ("XXII ARABIAN-ONENIGHTSTAND", 15))
+credit1 = Label(root, text = "-By Your Name ❤", background  = "lightgray", foreground = "black", font = ("Arial", 12))
 credit1.pack(anchor = "ne" , padx= 10)
 
 #--------------------------Link----------------------
-lbl_link = Label(root, text = "Link", background  = "gray3", foreground = "orange", font = ("Valorant", 10))
+lbl_link = Label(root, text = "Link", background  = "lightgray", foreground = "blue", font = ("Arial", 10))
 lbl_link.pack(anchor = "nw", padx= 50, pady= 20)
 
 ent_url = StringVar()
@@ -69,38 +65,15 @@ ent_link = Entry(root, width = 52, textvariable= ent_url)
 ent_link.place(x = 120, y = 105)
 
 #--------------------------Browse----------------------
-lbl_path = Label(root, text = "Path", background  = "gray3", foreground = "orange", font = ("Valorant", 10))
+lbl_path = Label(root, text = "Path", background  = "lightgray", foreground = "blue", font = ("Arial", 10))
 lbl_path.pack(anchor = "nw", padx= 50, pady= 20)
 
-btn_path = Button(root, text = "Browse", command = browsefile)
+btn_path = Button(root, text = "Browse", command = browse_directory)
 btn_path.place(x = 120, y = 160)
 
 #--------------------------Quality---------------------
-lbl_reso = Label(root, text = "Select Quality", background  = "gray3", foreground = "orange", font = ("Valorant", 10))
+lbl_reso = Label(root, text = "Select Quality", background  = "lightgray", foreground = "blue", font = ("Arial", 10))
 lbl_reso.pack(anchor= "nw", padx= 50, pady=20)
 
-lbl_war = Label(root, text= "Note: By default the quality will be Highest", background  = "gray3", foreground = "white", font = ("Consolas", 8))
-lbl_war.pack(anchor= "nw", padx= 50)
-
-var = IntVar()
-s = ttk.Style()                   
-s.configure('Wild.TRadiobutton', background= "gray3", foreground='orange') 
-
-res1 = Radiobutton(root, text= "High", variable= var, value=0, style= 'Wild.TRadiobutton')
-res1.place(x= 200, y= 220)
-
-res2 = Radiobutton(root, text= "Low", variable= var, value=1, style= 'Wild.TRadiobutton')
-res2.place(x= 300, y= 220)
-
-res3 = Radiobutton(root, text= "Audio", variable= var, value=2, style= 'Wild.TRadiobutton')
-res3.place(x= 400, y= 220)
-
-#--------------------------Download!---------------------
-s1 = ttk.Style()
-s1.configure('Wild.TButton', background = "gray3", foreground = "red", font = ("Exotc350 Bd BT Bold", 15, "bold"))
-
-btn_down = Button(root, text= "Download", style= 'Wild.TButton', command= download)
-btn_down.pack(anchor= "center", pady= 25)
-
-
-root.mainloop() 
+lbl_war = Label(root, text= "Note: By default the quality will be Highest", background  = "lightgray", foreground = "black", font = ("Arial", 8))
+lbl_war.pack(anchor= "nw
