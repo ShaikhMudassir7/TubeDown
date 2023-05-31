644cc6f4a421af70595e8a68c753850fbecbd8c6
Shaikh Mudassir <send2mudassir@gmail.com>
2023-05-31 23:37:56 +0530

    MDApp

--- yt.py
+++ yt.py
@@ -1,79 +1,106 @@
-from os import path
-from tkinter import * 
-from tkinter import messagebox
-from tkinter import filedialog
-from tkinter import ttk
+from kivymd.app import MDApp
+from kivymd.uix.button import MDFlatButton
+from kivymd.uix.label import MDLabel
+from kivymd.uix.textfield import MDTextField
+from kivymd.uix.boxlayout import BoxLayout
+from kivymd.uix.filemanager import MDFileManager
+from kivymd.uix.dialog import MDDialog
 from pytube import YouTube
 
-selected_path = ""
-def browse_directory():
-    global selected_path
-    selected_path = filedialog.askdirectory()
-    lbl_path_show = Label(root, text= "Path: "+ str(selected_path), background = "lightgray", foreground = "black", font = ("Arial", 10, "bold"))
-    lbl_path_show.place(x = 200, y = 162)
-    if len(selected_path) < 1:
-        messagebox.showerror("Error", "Please insert Directory") 
+class VideoDownloaderApp(MDApp):
+    selected_path = ""
+    selected_resolution = ""
 
-def download_video():
-    url = str(ent_link.get())
-    resolution = var.get()
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.url_textfield = None
+        self.resolution_textfield = None
 
-    if len(url) < 1:
-        messagebox.showerror("Error", "URL cannot be Empty")
- 
-    yt = YouTube(url)
-    try:
-        if var.get() == 0:
-             selected_resolution = yt.streams.get_highest_resolution()
-        elif var.get() == 1:
-            selected_resolution = yt.streams.get_lowest_resolution()
-        elif var.get() == 2:
-            selected_resolution = yt.streams.filter(only_audio=True).first()
-        else:
-            selected_resolution = yt.streams.get_highest_resolution()
+    def build(self):
+        self.theme_cls.theme_style = "Light"
+        self.theme_cls.primary_palette = "BlueGray"
+
+        main_layout = BoxLayout(orientation="vertical", spacing="10dp", padding="10dp")
+
+        heading_label = MDLabel(text="Video Downloader", font_style="H5", theme_text_color="Secondary")
+        main_layout.add_widget(heading_label)
+
+        self.url_textfield = MDTextField(hint_text="URL", required=True)
+        main_layout.add_widget(self.url_textfield)
+
+        browse_button = MDFlatButton(text="Browse", on_release=self.browse_directory)
+        main_layout.add_widget(browse_button)
+
+        self.resolution_textfield = MDTextField(hint_text="Select Quality", required=True)
+        main_layout.add_widget(self.resolution_textfield)
+
+        download_button = MDFlatButton(text="Download", on_release=self.download_video)
+        main_layout.add_widget(download_button)
+
+        return main_layout
+
+    def browse_directory(self, instance):
+        manager = MDFileManager(
+            exit_manager=self.exit_manager,
+            select_path=self.select_path
+        )
+        manager.show('/')
+
+    def exit_manager(self, *args):
+        self.file_manager.close()
+
+    def select_path(self, path):
+        self.selected_path = path
+        self.file_manager.close()
+
+    def download_video(self, instance):
+        url = self.url_textfield.text
+        resolution = self.resolution_textfield.text
+
+        if not url or not resolution:
+            self.show_error_dialog("Error", "URL and Resolution are required.")
+            return
+
         try:
-            selected_resolution.download(selected_path)
-            
-            messagebox.showinfo("Success", "Video Downloaded!")
-            messagebox.showinfo("Message", "For more information, please refer to the documentation.")
+            yt = YouTube(url)
+            if resolution.lower() == "highest":
+                self.selected_resolution = yt.streams.get_highest_resolution()
+            elif resolution.lower() == "lowest":
+                self.selected_resolution = yt.streams.get_lowest_resolution()
+            elif resolution.lower() == "audio":
+                self.selected_resolution = yt.streams.filter(only_audio=True).first()
+            else:
+                self.show_error_dialog("Error", "Invalid resolution.")
+                return
 
-        except:
-            messagebox.showerror("Error", "Download Failed")
-    except:
-        messagebox.showerror("Error","Please try again")
+            self.selected_resolution.download(self.selected_path)
 
+            self.show_info_dialog("Success", "Video Downloaded!")
+        except Exception as e:
+            self.show_error_dialog("Error", str(e))
 
-root = Tk()
-root.geometry('500x400+350+100')
-root.resizable(False, False)
-root.title("Video Downloader")
-root.config(bg = "lightgray")
+    def show_error_dialog(self, title, text):
+        dialog = MDDialog(
+            title=title,
+            text=text,
+            buttons=[
+                MDFlatButton(text="OK", on_release=self.close_dialog)
+            ]
+        )
+        dialog.open()
 
-#--------------------------Header----------------------
-heading = Label(root, text = "Video Downloader", background  = "lightgray", foreground = "blue", font = ("Arial", 15))
-heading.pack(anchor= "center", pady = 10)
+    def show_info_dialog(self, title, text):
+        dialog = MDDialog(
+            title=title,
+            text=text,
+            buttons=[
+                MDFlatButton(text="OK", on_release=self.close_dialog)
+            ]
+        )
+        dialog.open()
 
-credit1 = Label(root, text = "-By Your Name ❤", background  = "lightgray", foreground = "black", font = ("Arial", 12))
-credit1.pack(anchor = "ne" , padx= 10)
+    def close_dialog(self, instance):
+        instance.parent.parent.parent.dismiss()
 
-#--------------------------Link----------------------
-lbl_link = Label(root, text = "Link", background  = "lightgray", foreground = "blue", font = ("Arial", 10))
-lbl_link.pack(anchor = "nw", padx= 50, pady= 20)
-
-ent_url = StringVar()
-ent_link = Entry(root, width = 52, textvariable= ent_url)
-ent_link.place(x = 120, y = 105)
-
-#--------------------------Browse----------------------
-lbl_path = Label(root, text = "Path", background  = "lightgray", foreground = "blue", font = ("Arial", 10))
-lbl_path.pack(anchor = "nw", padx= 50, pady= 20)
-
-btn_path = Button(root, text = "Browse", command = browse_directory)
-btn_path.place(x = 120, y = 160)
-
-#--------------------------Quality---------------------
-lbl_reso = Label(root, text = "Select Quality", background  = "lightgray", foreground = "blue", font = ("Arial", 10))
-lbl_reso.pack(anchor= "nw", padx= 50, pady=20)
-
-lbl_war = Label(root, text= "Note: By default the quality will be Highest", background  = "lightgray", foreground = "black", font = ("Arial", 8))
-lbl_war.pack(anchor= "nw
+if __name__ == "__main__":
+    VideoDownloaderApp().run()
